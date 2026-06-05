a533017d3651fe81a349a5213abf8c2ec0594ced
aryseenovate <aryan.saremi@seenovate.com>
2026-06-05 10:54:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -52,7 +52,7 @@ if ingredients_list:
 
   for ingredient_chosen in ingredients_list:
     ingredients_list += ingredient_chosen + ' '
-    st.subheader(fruit_chosen + ' Nutrition Information'
+    st.subheader(fruit_chosen + ' Nutrition Information')
     smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
     #st.text(smoothiefroot_response.json())
     sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
